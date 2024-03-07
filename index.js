c40361d0d3c96515e592671eb0d22e8065a679b3
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-07 17:50:39 +0330

    Project started.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middlewares
+app.use(require("cors")());
+app.use(express.json());
+
+// Routes
+app.use("/api/v1", require("./src/routes"));
+
+// Dotenv config
+require("dotenv").config();
+
+// DB Start
+const { MONGODB_URI } = process.env;
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log(`Connected to ${MONGODB_URI}.`);
+});
+
+// App start
+const { APP_PORT, APP_HOSTNAME } = process.env;
+app.listen(APP_PORT, APP_HOSTNAME, () => {
+  console.log(
+    `App is running on port: ${APP_PORT} and hostname: ${APP_HOSTNAME}.`
+  );
+});
