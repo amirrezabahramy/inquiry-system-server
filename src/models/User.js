17902cffc39dbc2d6d8ddb624057c70ae3cf09a4
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-07 19:15:15 +0330

    Created model for users

---
+++ src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+    },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
+
+const model = mongoose.model("User", schema);
+
+module.exports = model;
