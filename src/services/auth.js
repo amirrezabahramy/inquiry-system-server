49a3648e85397578dad621d03de5168ce300c784
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-07 22:51:40 +0330

    Added new function for verifying token

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,6 +2,12 @@ const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
-exports.generateAccessToken = function (obj) {
-  return jwt.sign(obj, process.env.APP_TOKEN_SECRET, { expiresIn: "1h" });
+exports.generateAccessToken = function (object) {
+  return jwt.sign(object, process.env.APP_TOKEN_SECRET_KEY, {
+    expiresIn: "1h",
+  });
+};
+
+exports.verifyToken = function (token, callback) {
+  return jwt.verify(token, process.env.APP_TOKEN_SECRET_KEY, callback);
 };
