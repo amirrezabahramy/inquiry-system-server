acf8ab17f468125661f51f96f4be44425e30bcbf
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-11 12:08:48 +0330

    Fixed a typo issue

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -11,12 +11,12 @@ const { authByRoles } = require("../middlewares/auth");
 const router = require("express").Router();
 
 router.route("/").get(authByRoles("admin"), getTickets);
-router.route("/:ticketId/receiverUsers").get(getTicketReceiverUsers);
+router.route("/:ticketId/receiver-users").get(getTicketReceiverUsers);
 router
   .route("/:ticketId/answer")
   .patch(authByRoles("admin", "user"), answerTicket);
 router
-  .route("/:ticketId/receiverUsers/:receiverUserId/replies")
+  .route("/:ticketId/receiver-users/:receiverUserId/replies")
   .get(getTicketReceiverReplies);
 router
   .route("/create-and-broadcast")
