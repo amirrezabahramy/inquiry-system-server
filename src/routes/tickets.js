4eb173333e35cefab645981d906751888741bf60
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-12 14:04:42 +0330

    Completed role based authentication and access controls, added setUser middleware after authentication

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,25 +1,38 @@
 const {
   getTicketsList,
   getTicketReceiverUsers,
-  getTicketReceiverReplies,
+  getTicketReceiverUserReplies,
   createAndBroadcastTicket,
   answerTicket,
 } = require("../controllers/tickets");
 
 const { authByRoles } = require("../middlewares/auth");
+const { setUser } = require("../middlewares/basic");
 
 const router = require("express").Router();
 
-router.route("/list").get(authByRoles("admin"), getTicketsList);
-router.route("/:ticketId/receiver-users").get(getTicketReceiverUsers);
 router
-  .route("/:ticketId/answer")
-  .patch(authByRoles("admin", "user"), answerTicket);
+  .route("/list")
+  .get(authByRoles("admin", "user"), setUser, getTicketsList);
+
+router
+  .route("/:ticketId/receiver-users")
+  .get(authByRoles("admin"), getTicketReceiverUsers);
+
 router
   .route("/:ticketId/receiver-users/:receiverUserId/replies")
-  .get(getTicketReceiverReplies);
+  .get(authByRoles("admin"), setUser, getTicketReceiverUserReplies);
+
+router
+  .route("/:ticketId/replies")
+  .get(authByRoles("user"), setUser, getTicketReceiverUserReplies);
+
 router
   .route("/create-and-broadcast")
-  .post(authByRoles("admin"), createAndBroadcastTicket);
+  .post(authByRoles("admin"), setUser, createAndBroadcastTicket);
+
+router
+  .route("/:ticketId/answer")
+  .patch(authByRoles("admin", "user"), setUser, answerTicket);
 
 module.exports = router;
