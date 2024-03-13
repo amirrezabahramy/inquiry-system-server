22e75acfecc191f2f25c71293d6f08f395a17158
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-13 17:34:20 +0330

    Added filter

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,16 @@
 const { usersOptions } = require("../controllers/users");
 
 const { authByRoles } = require("../middlewares/auth");
+const { applyClientFilter } = require("../middlewares/basic");
 
 const router = require("express").Router();
 
-router.route("/options").get(authByRoles("admin"), usersOptions);
+router
+  .route("/options")
+  .get(
+    authByRoles("admin"),
+    applyClientFilter("firstName", "lastName", "username"),
+    usersOptions
+  );
 
 module.exports = router;
