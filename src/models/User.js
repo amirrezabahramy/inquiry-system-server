04503327e7e5046ae84a240df1d77e5cdd4b437d
amirrezabahramy <amirrezabahramy@gmail.com>
2024-03-17 17:50:17 +0330

    Added regex validation for email field

--- src/models/User.js
+++ src/models/User.js
@@ -13,6 +13,10 @@ const schema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address.",
+      ],
     },
     username: {
       type: String,
