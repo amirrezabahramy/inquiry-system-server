905297687e29ab12b186aba85c5aa4e1ba9ef315
amirrezabahramy <amirrezabahramy@gmail.com>
2024-04-09 09:29:19 +0330

    Added cors config

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { StatusCodes } = require("http-status-codes");
 const { logger } = require("./src/middlewares/basic");
 
 const app = express();
 
 // Middlewares
-app.use(require("cors")({ origin: process.env.CORS_ORIGIN }));
+app.use(
+  require("cors")({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    optionsSuccessStatus: StatusCodes.OK,
+  })
+);
 app.use(express.json());
 app.use(logger);
 
