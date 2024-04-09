a4054f35c246a670ef41e4a76bb853c6b48d0b8f
amirrezabahramy <amirrezabahramy@gmail.com>
2024-04-09 17:19:54 +0330

    Changed rate limit

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(
 );
 app.use(
   rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit: 100,
+    windowMs: 1 * 60 * 1000,
+    limit: 35,
     standardHeaders: "draft-7",
     legacyHeaders: false,
   })
