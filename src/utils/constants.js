c78e8b93ac793d365aa3bdf789c3d56655c7aa9f
amirrezabahramy <amirrezabahramy@gmail.com>
2024-06-21 19:19:14 +0330

    Added accepted file formats

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+exports.acceptedDocFormats = [
+  "xlsx",
+  "xlsm",
+  "xlsb",
+  "xltx",
+  "xltm",
+  "xls",
+  "xml",
+  "xlam",
+  "xla",
+  "xlw",
+  "xlr",
+  "csv",
+  "txt",
+];
+
+exports.acceptedPicFormats = ["jpg", "jpeg", "png", "webp"];
