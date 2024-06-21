a9f7bff5c9c48cfffc4965d21acb3f580081f0cc
amirrezabahramy <amirrezabahramy@gmail.com>
2024-06-21 20:28:37 +0330

    Fixed a bug in base 64 validator fn

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,21 +1,29 @@
 exports.validateBase64File = function (acceptedFormats, limitSize = 5) {
   return async function (value = "") {
-    const base64ContentArray = value.split(",");
-    if (base64ContentArray.length !== 2) {
+    try {
+      const { fileTypeFromBuffer } = await import("file-type");
+
+      const base64ContentArray = value.split(",");
+      if (base64ContentArray.length !== 2) {
+        return false;
+      }
+      const base64Content = base64ContentArray[1];
+      const binaryContent = Buffer.from(base64Content, "base64");
+      if (binaryContent.toString("base64") !== base64Content) {
+        return false;
+      }
+      const fileSizeInMB = binaryContent.length / (1024 * 1024);
+      if (fileSizeInMB > limitSize) {
+        return false;
+      }
+      await fileTypeFromBuffer(binaryContent);
+      return (
+        fileTypeResult &&
+        fileTypeResult.ext &&
+        acceptedFormats.includes(fileTypeResult.ext)
+      );
+    } catch (error) {
       return false;
     }
-    const base64Content = base64ContentArray[1];
-    const binaryContent = Buffer.from(base64Content, "base64");
-    if (binaryContent.toString("base64") !== base64Content) {
-      return false;
-    }
-    const fileSizeInMB = binaryContent.length / (1024 * 1024);
-    if (fileSizeInMB > limitSize) {
-      return false;
-    }
-    const fileTypeResult = await require("file-type").fileTypeFromBuffer(
-      binaryContent
-    );
-    return fileTypeResult && acceptedFormats.includes(fileTypeResult.ext);
   };
 };
