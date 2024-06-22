8d04524d9f9bf82d90f89f39b1d8a952b39681f3
amirrezabahramy <amirrezabahramy@gmail.com>
2024-06-22 19:45:17 +0330

    Added new "download" route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 router.use("/auth", require("./auth"));
 router.use("/inquiries", require("./inquiries"));
 router.use("/users", require("./users"));
+router.use("/download", require("./download"));
 router.use("/critical", require("./critical"));
 
 module.exports = router;
